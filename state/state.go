94fdca5cc951a8d77c05ecfd145e838ad3cb6c35
Anton Kaliaev <anton.kalyaev@gmail.com>
2018-07-30 12:59:16 +0400

    add proposer address to block's Header
    Refs #1134

--- state/state.go
+++ state/state.go
@@ -101,7 +101,8 @@ func (state State) GetValidators() (last *types.ValidatorSet, current *types.Val
 //------------------------------------------------------------------------
 // Create a block from the latest state
 
-// MakeBlock builds a block from the current state with the given txs, commit, and evidence.
+// MakeBlock builds a block from the current state with the given txs, commit,
+// and evidence.
 func (state State) MakeBlock(
 	height int64,
 	txs []types.Tx,
@@ -122,6 +123,8 @@ func (state State) MakeBlock(
 	block.ConsensusHash = state.ConsensusParams.Hash()
 	block.AppHash = state.AppHash
 	block.LastResultsHash = state.LastResultsHash
+
+	block.ProposerAddress = state.Validators.GetProposer().Address
 
 	return block, block.MakePartSet(state.ConsensusParams.BlockGossip.BlockPartSizeBytes)
 }
