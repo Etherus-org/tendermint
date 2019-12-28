0246f4b315ad2f67947541aed358be1c52d935d8
Dmitry Kochin <dco@mail.ru>
2019-12-28 14:19:29 +0300

    Increment fix - with future block proof

--- version/version.go
+++ version/version.go
@@ -4,13 +4,13 @@ package version
 const (
 	Maj = "0"
 	Min = "22"
-	Fix = "109"
+	Fix = "110"
 )
 
 var (
 	// Version is the current version of Tendermint
 	// Must be a string because scripts like dist.sh read this file.
-	Version = "0.22.109"
+	Version = "0.22.110"
 
 	// GitCommit is the current HEAD set using ldflags.
 	GitCommit string
